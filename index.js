c000603beee0871ac644be148c46355c44ac5bda
Quan Tien <quantien.nguyenvan@email.com>
2023-04-12 19:04:10 +0700

    login

--- index.js
+++ index.js
@@ -66,8 +66,8 @@ let products = {
 };
 
 for (let i of products.data) {
-  let box = document.createElement("div");
-  box.classList.add("card");
+  let card = document.createElement("div");
+  card.classList.add("card");
 
   let imgContainer = document.createElement("div");
   imgContainer.classList.add("image-container");
@@ -82,4 +82,28 @@ for (let i of products.data) {
 
   let name = document.createElement("h5");
   name.classList.add("product-name");
+  name.innerText = i.productName.toUpperCase();
+  container.appendChild(name);
+
+  let price = document.createElement("h6");
+  price.innerText = "$" + i.price;
+  container.appendChild(price);
+
+  card.appendChild(container);
+  document.getElementById("products").appendChild(card);
 }
+
+document.getElementById("search").addEventListener("click", () => {
+  let searchInput = document.getElementById("search-input").value.toUpperCase();
+  let cards = document.querySelectorAll(".card");
+  let productsName = document.querySelectorAll(".product-name");
+  let prices = document.querySelectorAll("h6");
+
+  productsName.forEach((items, index) => {
+    if (items.innerText.includes(searchInput)) {
+      cards[index].classList.remove("hide");
+    } else {
+      cards[index].classList.add("hide");
+    }
+  });
+});
