f10bceee24cd272b120f2eed4fa94d1d5728ae14
datngt2007 <dattulen15012007@gmail.com>
2023-04-12 20:42:45 +0700

    add

--- search.js
+++ search.js
@@ -1,16 +1,48 @@
-document
-document.getElementById("search").addEventListener("click", () => {
-  let searchInput = document.getElementById("search-input").value.toUpperCase();
-  let img = document.querySelectorAll(".img");
-  let productsName = document.querySelectorAll(".item-name");
-  let card = document.querySelectorAll(".product-lol");
-  
-  productsName.forEach((items, index) => {
-      if (items.innerText.toUpperCase().includes(searchInput)) {
-          card[index].classList.remove("hide");
-      } else {
-          card[index].classList.add("hide");
-      }
-  });
-});
+// document;
+// document.getElementById("search").addEventListener("click", () => {
+//   let searchInput = document.getElementById("search-input").value.toUpperCase();
+//   let img = document.querySelector(".img");
+//   let productsName = document.querySelector(".item-name");
+//   let card = document.querySelector(".product-lol");
 
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "78a198c0edmshfc7972957faa84cp1060fejsn663dc93fdacd",
+    "X-RapidAPI-Host": "youtube-v38.p.rapidapi.com",
+  },
+};
+
+fetch("https://youtube-v38.p.rapidapi.com/search/?q=mindx&hl=en&gl=US", options)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.contents);
+    for (let i of data.contents) {
+      console.log(i);
+      let card = document.createElement("div");
+      card.classList.add("card");
+
+      let imgContainer = document.createElement("div");
+      imgContainer.classList.add("image-container");
+
+      let image = document.createElement("img");
+      // image.setAttribute("src", i.image);
+      imgContainer.appendChild(image);
+      card.appendChild(imgContainer);
+
+      let container = document.createElement("div");
+      container.classList.add("container");
+
+      let name = document.createElement("h5");
+      name.classList.add("product-name");
+      // name.innerText = i.productName.toUpperCase();
+      container.appendChild(name);
+
+      let price = document.createElement("h6");
+      // price.innerText = "$" + i.price;
+      container.appendChild(price);
+
+      card.appendChild(container);
+      document.getElementById("ax").appendChild(card);
+    }
+  });
