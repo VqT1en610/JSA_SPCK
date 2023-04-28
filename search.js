ef078e03c54c9ae02750848fa387313c9aa42b42
Trần Đỗ Anh Khoa <anhkhoatrando123@gmail.com>
2023-04-28 17:55:32 +0700

    new code:

--- search.js
+++ search.js
@@ -1,50 +1,46 @@
 const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "78a198c0edmshfc7972957faa84cp1060fejsn663dc93fdacd",
-    "X-RapidAPI-Host": "youtube-v38.p.rapidapi.com",
-  },
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "78a198c0edmshfc7972957faa84cp1060fejsn663dc93fdacd",
+        "X-RapidAPI-Host": "youtube-v38.p.rapidapi.com"
+    }
 };
 
 document.getElementById("search").addEventListener("click", () => {
-  fetch(
-    "https://youtube-v38.p.rapidapi.com/search/?q=childrens%20days%20&hl=en&gl=US",
-    options
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data.contents);
+    document.getElementById("ax").innerHTML = "";
 
-      for (let i of data.contents.slice(0, 10)) {
-        console.log(i);
-        let card = document.createElement("div");
-        card.classList.add("card");
+    let input = document.getElementById("search-input").value;
 
-        let imgContainer = document.createElement("div");
-        imgContainer.classList.add("container");
+    fetch(`https://youtube-v38.p.rapidapi.com/search/?q=${input}%20for%20kids&hl=en&gl=US`, options)
+        .then((response) => response.json())
+        .then((data) => {
+            for (let i of data.contents.slice(0, 12)) {
+                let card = document.createElement("div");
+                card.classList.add("card");
 
-        let image = document.createElement("img");
-        image.setAttribute("src", i.image);
-        imgContainer.appendChild(image);
-        card.appendChild(imgContainer);
+                let imgContainer = document.createElement("div");
+                imgContainer.classList.add("container");
 
-        let container = document.createElement("div");
-        container.classList.add("container");
+                let image = document.createElement("img");
+                image.setAttribute("src", i.image);
+                imgContainer.appendChild(image);
+                card.appendChild(imgContainer);
 
-        let name = document.createElement("h5");
-        name.classList.add("product-name");
-        // name.innerText = i.productName.toUpperCase();
-        imgContainer.appendChild(name);
+                let container = document.createElement("div");
+                container.classList.add("container");
 
-        // let price = document.createElement("h6");
-        // // price.innerText = "$" + i.price;
-        // container.appendChild(price);
+                let name = document.createElement("h5");
+                name.classList.add("product-name");
 
-        card.appendChild(container);
-        document.getElementById("ax").appendChild(card);
+                imgContainer.appendChild(name);
 
-        name.innerText = i.video.title;
-        image.src = i.video.thumbnails[0].url;
-      }
-    });
+                card.appendChild(container);
+                document.getElementById("ax").appendChild(card);
+
+                name.innerText = i.video.title;
+                image.src = i.video.thumbnails[0].url;
+            }
+        });
 });
+
+document.getElementById("search").click();
